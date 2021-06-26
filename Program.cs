3b21da91b32ac3f4ca7e8a5861cfa669b3fd66e1
JeffHason <67012500+KyberJeffHason@users.noreply.github.com>
2021-06-26 23:36:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int power = 2;
+            powerArray(myArray, power);
+        }
+
+        public static int[] powerArray(int[] arr, int power)
+        {
+            int[] ourArray = arr;
+
+            var newArray = ourArray
+                .Select(h => (int)Math.Pow(h, power));
+
+            return newArray.ToArray();
+        }
+    }
+}
